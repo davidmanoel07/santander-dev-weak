abd8ff18561d406dbc5d2d41a4804d325cef594b
davidmanoel07 <david.manoel0711@gmail.com>
2025-03-06 20:01:36 -0300

    Initial commit

---
+++ src/test/java/me/dio/santander_dev_weak/SantanderDevWeakApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_weak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
